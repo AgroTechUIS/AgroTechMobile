db7a6b63c60edd8147b7c333c0c6412fe17de8a3
SergioSanchez <sergio.sanchez@hexasolutions.co>
2023-08-18 05:52:54 -0500

    ADD - # se agregan widgets comunes

---
+++ lib/common_utilities/widgets/search_bar_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key, this.searchText});
+
+  final String? searchText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: const Color.fromRGBO(255, 255, 255, 0.9),
+      ),
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.search),
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: searchText ?? 'Buscar',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
