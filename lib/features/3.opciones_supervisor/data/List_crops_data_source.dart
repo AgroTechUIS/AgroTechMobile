41abc1d38f8d95b94a70f5c4bac1efdf1522fd77
SergioSanchez <sergio.sanchez@hexasolutions.co>
2023-10-25 22:06:57 -0500

    ADD - # servicio de cultivos

--- lib/features/3.opciones_supervisor/data/List_crops_data_source.dart
+++ lib/features/3.opciones_supervisor/data/List_crops_data_source.dart
@@ -3,6 +3,7 @@ import 'package:agrotech/common_utilities/httpResponseModel.dart';
 import 'package:agrotech/common_utilities/route_service.dart';
 import 'package:agrotech/features/3.opciones_supervisor/domain/models/company_model.dart';
 import 'package:agrotech/features/3.opciones_supervisor/domain/models/crop_model.dart';
+import 'package:agrotech/features/3.opciones_supervisor/domain/models/new_activity_model.dart';
 
 class CropsDataSource {
   final ClientHttp clienthttp = ClientHttp();
@@ -13,5 +14,13 @@ class CropsDataSource {
 
   Future<HttpResponseModel> loadActivitiesCrop({required CropModel cropModel}) async {
     return clienthttp.post(url: '${RouteService.routeService}/actividad-cultivo', body: cropModel.toJson());
+  }
+
+  Future<HttpResponseModel> loadListWorkers({required String id}) async {
+    return clienthttp.get(url: '${RouteService.routeService}/api/obreros/11');
+  }
+
+  Future<HttpResponseModel> newActivity({required NewActivityModel newActivity}) async {
+    return clienthttp.post(url: '${RouteService.routeService}/api/actividades/nueva', body: newActivity.toJson());
   }
 }
--- lib/features/3.opciones_supervisor/domain/use_cases/list_crops_use_case.dart
+++ lib/features/3.opciones_supervisor/domain/use_cases/list_crops_use_case.dart
@@ -1,12 +1,18 @@
+import 'package:agrotech/common_utilities/models/list_response_model.dart';
+import 'package:agrotech/common_utilities/models/response_model.dart';
 import 'package:agrotech/features/3.opciones_supervisor/data/List_crops_data_source.dart';
 import 'package:agrotech/features/3.opciones_supervisor/domain/models/activity_crop_response_model.dart';
 import 'package:agrotech/features/3.opciones_supervisor/domain/models/company_model.dart';
 import 'package:agrotech/features/3.opciones_supervisor/domain/models/crop_model.dart';
 import 'package:agrotech/features/3.opciones_supervisor/domain/models/crops_response_model.dart';
+import 'package:agrotech/features/3.opciones_supervisor/domain/models/new_activity_model.dart';
+import 'package:agrotech/features/3.opciones_supervisor/domain/models/worker_model.dart';
 
 abstract class CropsUseCase {
   Future<CropsResponseModel> loadCrops({required CompanyModel companyModel});
   Future<ActivityCropsResponseModel> loadActivitiesCrop({required CropModel cropModel});
+  Future<ResponseModel<List<WorkerModel>>> loadListWorkers({required String idEmpresa});
+  Future<ResponseModel<bool>> newActivity({required NewActivityModel newActivity});
 }
 
 class CropsUseCaseImpl extends CropsUseCase {
@@ -33,6 +39,31 @@ class CropsUseCaseImpl extends CropsUseCase {
       return ActivityCropsResponseModel.fromJson(response.body ?? {});
     } else {
       return ActivityCropsResponseModel(error: 'loadActivitiesCrop:success:false');
+    }
+  }
+
+  @override
+  Future<ResponseModel<List<WorkerModel>>> loadListWorkers({required String idEmpresa}) async {
+    var response = await listCropsDataSource.loadListWorkers(id: idEmpresa);
+    if (response.success) {
+      var respon = ListResponseModel.fromJson(
+        response.body ?? {},
+        (json) => WorkerModel.fromJson(json),
+        'obreros',
+      );
+      return ResponseModel(response: respon.list);
+    } else {
+      return ResponseModel(error: response.message);
+    }
+  }
+
+  @override
+  Future<ResponseModel<bool>> newActivity({required NewActivityModel newActivity}) async {
+    var response = await listCropsDataSource.newActivity(newActivity: newActivity);
+    if (response.success) {
+      return ResponseModel(response: response.success);
+    } else {
+      return ResponseModel(error: response.message);
     }
   }
 }
