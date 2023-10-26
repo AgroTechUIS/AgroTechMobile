3bfc2e99691c89ee6f96e0a71c8c352b16bf48b3
SergioSanchez <sergio.sanchez@hexasolutions.co>
2023-10-25 22:11:18 -0500

    ADD - # tema de aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:agrotech/common_utilities/config/colors_theme.dart';
 import 'package:agrotech/features/1.login/presentation/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,16 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AgroTech',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: 'login',
-      routes: {
-        'login': (_) => LoginPage(),
-        //'miapp': (_) => const MyPage(),
-      },
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'AgroTech',
+        initialRoute: 'login',
+        routes: {
+          'login': (_) => const LoginPage(),
+        },
+        theme: ThemeData.light().copyWith(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(colors.green3),
+            ),
+          ),
+          appBarTheme: AppBarTheme(color: colors.green3),
+        ));
   }
 }
