10e8218aae4f693b6defc682a7af5236f259d00b
SergioSanchez <sergio.sanchez@hexasolutions.co>
2023-10-25 22:13:32 -0500

    ADD - # se deja de usar configuracion de imagenes

--- lib/common_utilities/config/network_images.dart
+++ lib/common_utilities/config/network_images.dart
@@ -1,5 +1,5 @@
-class NetImages {
+/*class NetImages {
   final String background = 'https://img.freepik.com/free-vector/abstract-gray-stripes-background_1035-19089.jpg';
 }
 
-final netImages = NetImages();
+final netImages = NetImages();*/
