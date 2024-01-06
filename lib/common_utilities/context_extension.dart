9f6b414f766ba75f4472c4e5f9d7ce30db467019
SergioSanchez <sergio.sanchez@hexasolutions.co>
2024-01-05 19:58:08 -0500

    ADD - # mejora de navegacion

--- lib/common_utilities/context_extension.dart
+++ lib/common_utilities/context_extension.dart
@@ -5,4 +5,18 @@ extension ContextExtension on BuildContext {
   double rp(double pixel) {
     return pixel / 360 * MediaQuery.of(this).size.width;
   }
+
+  void pushRoute(Widget page) {
+    Navigator.push(
+      this,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+
+  void remplaceRoute(Widget page) {
+    Navigator.pushReplacement(
+      this,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
 }
